91058e251134406b764e077de7ff2e5b399b628f
hegavon <victorgavon@gmail.com>
2024-02-29 15:23:04 +0100

    Binary Trees header file update

--- binary_trees.h
+++ binary_trees.h
@@ -43,3 +43,5 @@ void binary_tree_print(const binary_tree_t *);
 
 /* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* _BINARY_TREES_H_ */
