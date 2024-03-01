3b1185d8e250c22d1bd494d1cd7471bea228696d
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-03-02 00:23:02 +0100

    Heap - Extract

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,35 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: value to put in the new node
- *
- * Return: a pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *node;
-
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
-		return (NULL);
-
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-
-	return (node);
-}
-
-/**
- * binary_tree_height - measures the height of a binary tree
+ * tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  *
  * Return: Height or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t tree_height(const heap_t *tree)
 {
 	size_t height_l = 0;
 	size_t height_r = 0;
@@ -38,10 +15,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		height_l = 1 + binary_tree_height(tree->left);
+		height_l = 1 + tree_height(tree->left);
 
 	if (tree->right)
-		height_r = 1 + binary_tree_height(tree->right);
+		height_r = 1 + tree_height(tree->right);
 
 	if (height_l > height_r)
 		return (height_l);
@@ -49,46 +26,92 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the parent node
- * @value: value to put in the new node
+ * _preorder - goes through a binary tree using pre-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @node: will be the last node in traverse
+ * @height: height of tree
  *
- * Return: a pointer to the created node, or NULL on failure
+ * Return: No Return
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+void _preorder(heap_t *tree, heap_t **node, size_t height)
 {
-	binary_tree_t *new_node;
+	if (!tree)
+		return;
 
-	if (parent == NULL)
-		return (NULL);
+	if (!height)
+		*node = tree;
+	height--;
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->left = parent->left;
-	if (new_node->left != NULL)
-		new_node->left->parent = new_node;
-	parent->left = new_node;
-
-	return (new_node);
+	_preorder(tree->left, node, height);
+	_preorder(tree->right, node, height);
 }
 
 /**
- * binary_tree_print - Prints a binary tree
- * @tree: Pointer to the root node of the tree to print
+ * heapify - heapifies max binary heap
+ * @root: pointer to binary heap
  */
-void binary_tree_print(const binary_tree_t *tree)
+void heapify(heap_t *root)
 {
-	if (tree == NULL)
+	int value;
+	heap_t *tmp1, *tmp2;
+
+	if (!root)
 		return;
 
-	printf("%d\n", tree->n);
-	if (tree->left != NULL || tree->right != NULL)
+	tmp1 = root;
+
+	while (1)
 	{
-		printf("Left:\n");
-		binary_tree_print(tree->left);
-		printf("Right:\n");
-		binary_tree_print(tree->right);
+		if (!tmp1->left)
+			break;
+		if (!tmp1->right)
+			tmp2 = tmp1->left;
+		else
+		{
+			if (tmp1->left->n > tmp1->right->n)
+				tmp2 = tmp1->left;
+			else
+				tmp2 = tmp1->right;
+		}
+		if (tmp1->n > tmp2->n)
+			break;
+		value = tmp1->n;
+		tmp1->n = tmp2->n;
+		tmp2->n = value;
+		tmp1 = tmp2;
 	}
+}
+
+/**
+ * heap_extract - extracts the root node from a Max Binary Heap
+ * @root: pointer to the heap root
+ * Return: value of extracted node
+ **/
+int heap_extract(heap_t **root)
+{
+	int value;
+	heap_t *heap_r, *node;
+
+	if (!root || !*root)
+		return (0);
+	heap_r = *root;
+	value = heap_r->n;
+	if (!heap_r->left && !heap_r->right)
+	{
+		*root = NULL;
+		free(heap_r);
+		return (value);
+	}
+
+	_preorder(heap_r, &node, tree_height(heap_r));
+
+	heap_r->n = node->n;
+	if (node->parent->right)
+		node->parent->right = NULL;
+	else
+		node->parent->left = NULL;
+	free(node);
+	heapify(heap_r);
+	*root = heap_r;
+	return (value);
 }
