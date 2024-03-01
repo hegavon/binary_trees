f720b569621f4fee5276597e2db529db3a948153
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-03-01 23:43:06 +0100

    Heap - Insert

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,6 +1,18 @@
 #include "binary_trees.h"
 
 /**
+ * swap_nodes - Swaps the values of two nodes.
+ * @node1: The first node.
+ * @node2: The second node.
+ */
+void swap_nodes(heap_t *node1, heap_t *node2)
+{
+	int temp = node1->n;
+	node1->n = node2->n;
+	node2->n = temp;
+}
+
+/**
  * heapify_up - Performs the "heapify-up" operation after insertion.
  * @node: The newly inserted node.
  */
@@ -8,9 +20,7 @@ void heapify_up(heap_t *node)
 {
 	while (node->parent && node->n > node->parent->n)
 	{
-		int temp = node->n;
-		node->n = node->parent->n;
-		node->parent->n = temp;
+		swap_nodes(node, node->parent);
 		node = node->parent;
 	}
 }
@@ -23,7 +33,7 @@ void heapify_up(heap_t *node)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *node, *parent, *temp;
+	heap_t *node, *parent;
 
 	if (!root)
 		return (NULL);
@@ -55,12 +65,6 @@ heap_t *heap_insert(heap_t **root, int value)
 
 	/* Perform heapify-up operation */
 	heapify_up(node);
-
-	/* Adjust root if necessary */
-	temp = node;
-	while (temp->parent)
-		temp = temp->parent;
-	*root = temp;
 
 	return (node);
 }
