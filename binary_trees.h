7e159f3e8fb9fd7f1c1a093a4639f76b15222c13
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-03-01 06:50:54 +0100

    BST - Insert

--- binary_trees.h
+++ binary_trees.h
@@ -87,5 +87,6 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif /* BINARY_TREES_H */
