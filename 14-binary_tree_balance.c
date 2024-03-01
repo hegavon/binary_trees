d46575faa27cdb90fd63c4bb39eb1dd42075cf1e
hegavon <victorgavon@gmail.com>
2024-03-01 02:32:53 +0100

    Binary Trees Task 14. Balance factor - files update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,10 +8,10 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
 }
 
 /**
@@ -22,12 +22,14 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
 
