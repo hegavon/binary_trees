dc5ce0f4688228128b538f30a9557399a451b333
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-03-02 02:00:47 +0100

    Heap - Sort

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,56 +1,52 @@
 #include "binary_trees.h"
 
 /**
- * tree_size - measures the total number of nodes in a binary tree
- * @tree: pointer to the root node of the tree to measure
+ * tree_size - The tree_size function measures the size of a tree
+ * @tree: The pointer to the root node of the tree to measure
  *
- * Return: The total number of nodes in the tree
+ * Return: Height or 0 if tree is NULL
  */
 size_t tree_size(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-	if (tree)
-	{
-		count += 1; /* Count the current node */
+	if (!tree)
+		return (0);
 
-		/* Recursively count nodes in the left subtree */
-		count += tree_size(tree->left);
+	if (tree->left)
+		height_l = 1 + tree_size(tree->left);
 
-		/* Recursively count nodes in the right subtree */
-		count += tree_size(tree->right);
-	}
+	if (tree->right)
+		height_r = 1 + tree_size(tree->right);
 
-	return (count);
+	return (height_l + height_r);
 }
 
 /**
- * heap_to_sorted_array - converts a Binary Max Heap
- * to a sorted array of integers
+ * heap_to_sorted_array - This function converts a heap to an array
  *
- * @heap: pointer to the root node of the heap to convert
- * @size: address to store the size of the array
+ * @heap: Ihe pointer to the root node of the heap to convert
+ * @size: The size of the array
  *
- * Return: pointer to array sorted in descending order,
- *         or NULL if heap is NULL or size is NULL
+ * Return: It returns a pointer to the array
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int i, *sorted_array = NULL;
+	int i, *a = NULL;
 
 	if (!heap || !size)
 		return (NULL);
 
-	*size = tree_size(heap); /* Calculate the size of the array */
+	*size = tree_size(heap) + 1;
 
-	sorted_array = malloc(sizeof(int) * (*size));
+	a = malloc(sizeof(int) * (*size));
 
-	if (!sorted_array)
+	if (!a)
 		return (NULL);
 
-	/* Extract elements from the heap and store them in the sorted array */
-	for (i = (*size) - 1; i >= 0; i--)
-		sorted_array[i] = heap_extract(&heap);
+	for (i = 0; heap; i++)
+		a[i] = heap_extract(&heap);
 
-	return (sorted_array);
+	return (a);
 }
